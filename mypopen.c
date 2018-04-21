5e335604a6476aff37de0eceed9202d42dce942a
ic17b022 <ic17b022@technikum-wien.at>
2018-04-21 15:07:15 +0200

    add basic error handling

--- mypopen.c
+++ mypopen.c
@@ -1,20 +1,27 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
-
+#include "mypopen.h"
 
 FILE *mypopen(const char *command, const char *type)
 {
     pid_t pid;
     int mypipe[2];
 
+    if (*type != 'w' && *type != 'r'){
+        errno = EINVAL;
+        return NULL;
+    }
 
-    pipe(mypipe);
+    if(pipe(mypipe) != 0)
+        return NULL;    //errno remains set
 
     pid = fork();
 
-    if (pid == (pid_t) 0) {
+    if (pid == -1){
+        return NULL;    //errno remains set
+    } else if (pid == (pid_t) 0) {
 
         /* child process
         *
@@ -37,12 +44,7 @@ FILE *mypopen(const char *command, const char *type)
             close(mypipe[0]);
             //set fd to std.out
             dup2(mypipe[1],stdout);
-        } else {
-            //wenn falsch
-            //Im Kindprozeß ist das richtige Ende der Pipe ("r" oder "w") mit stdin bzw. stdout zu assoziieren (dup2(2))
         }
-
-
     }
     else {
             // parent process
@@ -52,15 +54,7 @@ FILE *mypopen(const char *command, const char *type)
         } else if (*type == 'r') {
             close(mypipe[1]);
             return fdopen(mypipe[0],type);
-        } else {
-            //wenn falsch
-            //Im Kindprozeß ist das richtige Ende der Pipe ("r" oder "w") mit stdin bzw. stdout zu assoziieren (dup2(2))
         }
-
-        */
-
-
-
     }
 
 
@@ -70,22 +64,4 @@ FILE *mypopen(const char *command, const char *type)
 int mypclose(FILE *stream)
 {
     return pclose(stream);
-}
-
-void read_from_pipe (int file)
-{
-    FILE *stream;
-    int c;
-    stream = fdopen (file, "r");
-    while ((c = fgetc (stream)) != EOF)
-        // implement output of read data
-        fclose (stream);
-}
-
-void write_to_pipe (int file)
-{
-    FILE *stream;
-    stream = fdopen (file, "w");
-    //implement reading of data
-    fclose (stream);
 }
--- mypopen.h
+++ mypopen.h
@@ -1,12 +1,10 @@
 #ifndef MYPOPEN_H
 #define MYPOPEN_H
 
+#include <stdio.h>
+
 FILE *mypopen(const char *command, const char *type);
 
 int mypclose(FILE *stream);
-
-void read_from_pipe (int file);
-
-void write_to_pipe (int file);
 
 #endif
